23ee6fa9cf85d360be6534502a76e5e268b01c40
beatrizartimundo <beatriz.artimundo@gmail.com>
2022-02-07 15:10:50 -0300

    add css and last funcionalities

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import PageHeader from '../template/pageHeader';
 
 export default props =>(
   <div>
-    <h1>About</h1>
+    <PageHeader name="Sobre"></PageHeader>
+
+    <h2>Nossa Historia</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur cupiditate dolorum porro et architecto nobis. Animi, similique a, quibusdam quia quis ratione cum, placeat consequuntur magni amet accusantium. Non, eligendi.</p>
+    <h2>Missao e visao</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias laudantium dolorem, vel sint voluptas maiores maxime voluptate? Voluptatibus alias cum facere? Minima ut quibusdam, quas possimus eos similique sapiente at!</p>
   </div>
 )
